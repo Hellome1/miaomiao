0b0702e755d136112d7b003bc84d9c5a528973ae
Hellome1 <870191056@qq.com>
2021-03-21 19:23:26 +0800

    add createComponents

---
+++ src/components/City/index.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="city_body">
+    <div class="city_list">
+      <div class="city_hot">
+        <h2>热门城市</h2>
+        <ul class="clearfix">
+          <li>上海</li>
+          <li>成都</li>
+          <li>北京</li>
+          <li>北京</li>
+          <li>北京</li>
+          <li>北京</li>
+        </ul>
+      </div>
+      <div class="city_sort">
+        <div>
+          <h2>A</h2>
+          <ul>
+            <li>阿拉</li>
+            <li>鞍山</li>
+          </ul>
+        </div>
+        <div>
+          <h2>A</h2>
+          <ul>
+            <li>阿拉</li>
+            <li>鞍山</li>
+          </ul>
+        </div>
+        <div>
+          <h2>A</h2>
+          <ul>
+            <li>阿拉</li>
+            <li>鞍山</li>
+          </ul>
+        </div>
+        <div>
+          <h2>A</h2>
+          <ul>
+            <li>阿拉</li>
+            <li>鞍山</li>
+          </ul>
+        </div>
+        <div>
+          <h2>A</h2>
+          <ul>
+            <li>阿拉</li>
+            <li>鞍山</li>
+          </ul>
+        </div>
+        <div>
+          <h2>A</h2>
+          <ul>
+            <li>阿拉</li>
+            <li>鞍山</li>
+          </ul>
+        </div>
+      </div>
+      <div class="city_index">
+        <ul>
+          <li>A</li>
+          <li>B</li>
+          <li>C</li>
+          <li>D</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'City'
+}
+</script>
+
+<style lang="scss" scoped>
+.city_body {
+  background-color: rgba(223, 186, 192, .2);
+  .city_list {
+    .city_hot {
+      margin: 20px 0;
+      h2 {
+        font-size: 20px;
+        line-height: 36px;
+        padding-left: 10px;
+        font-weight: normal;
+        background-color: rgba(212, 212, 212, 0.5);
+      }
+      .clearfix {
+        overflow: hidden;
+        li {
+          float: left;
+          margin: 10px 0 0 16px;
+          width: 110px;
+          height: 36px;
+          line-height: 36px;
+          text-align: center;
+          border: 1px solid rgba(0, 0, 0, .3);
+        }
+      }
+    }
+    .city_sort {
+      div {
+        h2 {
+          font-size: 20px;
+          line-height: 36px;
+          padding-left: 10px;
+          font-weight: normal;
+          background-color: rgba(212, 212, 212, 0.5);
+        }
+        ul {
+          li {
+            margin: 10px;
+          }
+        }
+      }
+    }
+    .city_index {
+      position: fixed;
+      right: 0;
+      top: 51px;
+      height: 689px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: white;
+      width: 20px;
+    }
+  }
+}
+</style>
---
+++ src/components/Login/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="login_body">
+    <div>
+      <input type="text" class="login_text" placeholder="账户名/手机号/Email">
+    </div>
+    <div>
+      <input type="password" class="login_text" placeholder="请输入您的密码">
+    </div>
+    <div class="login_btn">
+      <input type="submit" value="登录">
+    </div>
+    <div class="login_link">
+      <a href="#">立即注册</a>
+      <a href="#">找回密码</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="scss" scoped>
+.login_body {
+  div {
+    margin: 20px 0;
+    .login_text {
+      height: 40px;
+      padding: 0 10px;
+      width: 100%;
+      outline: none;
+      border: none;
+      border-bottom: 1px solid rgba(0, 0, 0, .4);
+    }
+  }
+  .login_btn {
+    padding: 5px 10px;
+    input {
+      width: 100%;
+      height: 40px;
+      background-color: #f03d37;
+      border: none;
+      outline: none;
+      border-radius: 5px;
+    }
+  }
+  .login_link {
+    padding: 0 10px;
+    display: flex;
+    justify-content: space-between;
+    a {
+      font-size: 12px;
+      color: #f03d37;
+      text-decoration: none;
+    }
+  }
+}
+</style>
